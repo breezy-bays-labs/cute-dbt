9ce9abd96449077adf6e2cd7a212e9aad90c491c
cmbays <cmbays@users.noreply.github.com>
2026-06-11 06:13:55 +0000

    deploy: 4a61bf58eb01542aaa88be01f96c51c4f650c364

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e6dce51a.js',
+                path_to_root + 'searchindex-08e6ab1c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
